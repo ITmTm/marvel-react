4f3fb7fda747445ea465e835e3be9951962fc217
Timur <Stafr95@mail.ru>
2023-11-06 19:10:06 +0500

    creating const and export default

---
+++ src/components/app/App.js
@@ -0,0 +1,12 @@
+import AppHeader from '../appHeader/AppHeader';
+import RandomChar from '../randomChar/RandomChar';
+import CharList from '../charList/CharList';
+import CharInfo from '../charInfo/CharInfo';
+
+import decoration from '../../resources/img/vision.png'
+
+const App = () => {
+
+}
+
+export default App;
